f22ada67c0df8de72562021214be6ab457de3b71
+ <wanjirafaith22@gmail.com>
2023-07-22 19:49:14 +0300

    used use state and use effect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+    .then((res) => res.json())
+    .then((transactions) => setTransactions(transactions))
+  },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
+
