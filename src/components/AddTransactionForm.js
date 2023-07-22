76ec439dd660ef676e91b0051b69bc6f95eaa1e0
+ <wanjirafaith22@gmail.com>
2023-07-22 21:21:15 +0300

    added label for date, multiple input fields and a button for adding a transaction

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -17,8 +17,13 @@ function AddTransactionForm({onSubmittng}) {
      }
 
   return (
-    <form>
-      
+    <form onSubmit={handleSubmit} className='new-transaction' onChange={handleChange}>
+        <label htmlFor='date'>Date</label>
+        <input type='date' name='date' onChange={handleChange} value={formData.date}></input>
+        <input type='description' name='description' placeholder='Description' onChange={handleChange} value={formData.description}></input>
+        <input type='category'name='category' placeholder='Category' onChange={handleChange} value={formData.category}></input>
+        <input type='amount'name='amount' placeholder='Amount' onChange={handleChange} value={formData.amount}></input>
+        <button className='btn'>Add Transaction</button>
     </form>
   )
 }
