1b0ac48c38ca88b11d7b87a97a71e0090f073b82
+ <wanjirafaith22@gmail.com>
2023-07-22 20:10:12 +0300

    added a table on transaction list and mapped the transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,1 +1,31 @@
+import React from 'react'
+import Transaction from './Transaction';
+
+function TransactionList() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(item => {
+            return <Transaction 
+            key={item.id} 
+            date={item.date} 
+            description={item.description} 
+            category={item.category} 
+            amount={item.amount} >       
+            </Transaction>
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionList
 
